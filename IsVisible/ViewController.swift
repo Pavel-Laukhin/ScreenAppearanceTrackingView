09d0836029fc9027b9ab85ab0414346b1a295825
Pavel Laukhin <pavel.laukhin@homecredit.ru>
2023-01-25 16:08:00 +0700

    Initial Commit

---
+++ IsVisible/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IsVisible
+//
+//  Created by PAVEL LAUKHIN on 25.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
